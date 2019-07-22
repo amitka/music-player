8e9020e1ef0a65bf99c74d54da4d1f05b81c3c43
Amitka <amitka111@gmail.com>
2019-07-22 21:48:07 +0300

    player start stop added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,71 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Howl } from 'howler'; 
-//import SoundFile from './assets/sound.mp3';
+import SoundFile from './assets/sound.mp3';
 
-function App() {
-  const sound = new Howl({
-    src: [ require('./assets/sound.mp3') ]
-  });
-
-  const [ play, setPlay ] = useState(false);
+// export default class App extends React.Component {
+//   state= {
+//     sound: undefined,
+//     song: [ require('./assets/sound.mp3') ]
+//   }
   
-  useEffect(()=>{
-    let id = undefined
-    console.log('useEffect...');
-    if (play) {
-      console.log('play')
-      id = sound.play();
-      console.log(id);
-    }
-    else {
-      console.log('stop')
-      sound.stop([id]);
-    }
-  }, [play]);
+//   playSound = () => {
+//     console.log('play')
+//     this.state.sound.play();
+//   }
 
+//   stopSound = () => {
+//     console.log('stop')
+//     this.state.sound.stop();
+//   }
+
+//   componentDidMount(){
+//     const sound = new Howl({
+//       src: this.state.song
+//     });
+    
+//     this.setState({sound: sound});
+//   }
+  
+//   render() {
+//     return(
+//       <div className="App">
+//         <button onClick={ this.playSound }>Play</button>
+//         <button onClick={ this.stopSound }>Stop</button>
+//       </div>
+//     )
+//   }
+// }
+
+function Player(props) {
+  const [ sound, setSound ] = useState(undefined);
+  useEffect(() => {
+    const soundObj = new Howl({
+      src: props.song
+    });
+    setSound(soundObj)
+  },[])
+  
+  const [ play, setPlay ] = useState(false)
+  useEffect(() => {
+    if (sound === undefined) return
+    play ? sound.play() : sound.stop()
+  }, [play])
+
+  return(
+    <div className="player">
+      <button onClick={ () => setPlay(true) }>Play</button>
+      <button onClick={ () => setPlay(false) }>Stop</button>
+    </div>
+  )
+}
+
+export default function App() {
 
   return (
     <div className="App">
-      <button onClick={ () => setPlay(true) }>Play</button>
-      <button onClick={ () => setPlay(false) }>Stop</button>
+      <Player song={ SoundFile } />
     </div>
   );
 }
 
-export default App;
